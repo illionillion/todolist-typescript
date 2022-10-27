7b9fec5ca37fc1c37d30444a68b4b324bf99a92f
yuto ichinose <yuto.0526.sano@gmail.com>
2022-10-27 11:17:10 +0900

    mui 入れた 次はchakra ui使いたい

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import {
+  Button,
+  Checkbox,
+  IconButton,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
+import { Delete as DeleteIcon } from "@mui/icons-material";
 
 function App() {
-  
   const inputRef = useRef<HTMLInputElement>(null);
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState<Todo[]>([]); // Todo型の配列を持つように指定した
@@ -39,7 +50,7 @@ function App() {
   const returnDeepCopy = () => todos.map((todo) => ({ ...todo }));
   const handleEdit = (id: number, inputValue: string) => {
     /* ディープコピー(完全にコピーされた別の配列)に変えよう(ミューテートから守るため) */
-    const deepCopy = returnDeepCopy()
+    const deepCopy = returnDeepCopy();
 
     const newTodos = deepCopy.map((todo) => {
       if (todo.id === id) {
@@ -53,7 +64,7 @@ function App() {
   };
 
   const handleChecked = (id: number, checked: boolean) => {
-    const deepCopy = returnDeepCopy()
+    const deepCopy = returnDeepCopy();
 
     const newTodos = deepCopy.map((todo) => {
       if (todo.id === id) {
@@ -67,14 +78,14 @@ function App() {
   };
 
   const handleDelete = (id: number) => {
-    const deepCopy = returnDeepCopy()
+    const deepCopy = returnDeepCopy();
 
     // 該当するものを取り除く
-    const newTodos = deepCopy.filter((todo:Todo) => todo.id !== id);
+    const newTodos = deepCopy.filter((todo: Todo) => todo.id !== id);
     console.log(newTodos);
 
     setTodos(newTodos);
-  }
+  };
 
   // useEffect(()=>{ console.log(todos);
   //  },todos)
@@ -82,39 +93,103 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h2>To Do List with TypeScript</h2>
+        <Typography variant="h3" style={{margin:"20px"}}>To Do List with TypeScript</Typography>
         <form onSubmit={(e) => handleSubmit(e)}>
-          <input
+          {/* <input
             type="text"
             onChange={(e) => handleChange(e)}
             className="inputText"
             value={inputValue}
             ref={inputRef}
-          />
-          <input type="submit" value="追加" className="submitButton" />
+          /> */}
+          <TextField 
+            id="filled-basic"
+            label="Input To Do"
+            variant="outlined"
+            onChange={handleChange}
+            className="inputText"
+            value={inputValue}
+            />
+          {/* <input type="submit" value="追加" className="submitButton" /> */}
+          <Button variant="contained" type="submit" className="submitButton" style={{margin:"10px"}}>追加</Button>
         </form>
+        {/* <ul>
+        <ListItemButton>
+          <ListItemText primary="Spam" />
+        </ListItemButton>
+        </ul> */}
         <ul>
           {todos.map((todo) => (
             <li key={todo.id}>
-              <input
+              {/* <input
                 type="text"
                 onChange={(e) => handleEdit(todo.id, e.target.value)}
                 className="inputText"
                 value={todo.inputValue}
                 ref={inputRef}
                 disabled={todo.checked}
-              />
-              <input
+              /> */}
+              <TextField
+                id="standard-basic"
+                label="To Do"
+                variant="standard" 
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+                className="inputText"
+                value={todo.inputValue}
+                ref={inputRef}
+                disabled={todo.checked}
+                />
+              {/* <input
                 type="checkbox"
                 onChange={() => handleChecked(todo.id, todo.checked)}
                 className="inputText"
                 checked={todo.checked}
                 ref={inputRef}
-              />
-              <input type="button" onClick={()=>handleDelete(todo.id)} value="消" />
+              /> */}
+              <Checkbox 
+                onChange={() => handleChecked(todo.id, todo.checked)}
+                className="inputText"
+                checked={todo.checked}
+                size="small" />
+              {/* <input
+                type="button"
+                onClick={() => handleDelete(todo.id)}
+                value="消"
+              /> */}
+              <Button variant="outlined" onClick={ () => handleDelete(todo.id)}><DeleteIcon /></Button>
             </li>
           ))}
         </ul>
+        {/* <ListItem sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {[0, 1, 2, 3].map((value) => {
+        const labelId = `checkbox-list-label-${value}`;
+
+        return (
+          <ListItem
+                  key={value}
+                  secondaryAction={
+                    <IconButton  edge="end" aria-label="comments">
+                      <CommentIcon />
+                    </IconButton>
+                  }
+                  disablePadding
+                >
+                  <ListItemButton role={undefined} onClick={e=>{}} dense>
+                    <ListItemIcon>
+                      <Checkbox
+                        edge="start"
+                        checked={checked.indexOf(value) !== -1}
+                        tabIndex={-1}
+                        disableRipple
+                        inputProps={{ 'aria-labelledby': labelId }}
+                      />
+                    </ListItemIcon>
+                    <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
+                  </ListItemButton>
+                </ListItem>
+              );
+            })}
+          </ListItem> */}
       </div>
     </div>
   );
